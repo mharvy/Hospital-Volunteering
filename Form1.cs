ec197c17a364c3751c508cce06e4ea058981968e
mharvy <32440398+mharvy@users.noreply.github.com>
2018-06-22 15:37:07 -0500

    Added C# files for windows app via upload

---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsCVA
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown13_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the user filename and number of trials
+            string filename = "";
+            int trials = 0;
+            int sample_size = 0;
+            try
+            {
+                filename = filenameBox.Text;
+                trials = Convert.ToInt16(trialsBox.Text);
+                sample_size = Convert.ToInt16(sampleSizeBox.Text);
+            }
+            catch { MessageBox.Show("Error Message"); }  // Error func will go here
+
+            // This chunk gets the variables from the form, as two lists: one of all the first variables, and the 
+            // next as all the second variables. It also checks to make sure they are the same length.
+            // The two lists are var1DecList and var2DecList
+            List<string> curvars1 = new List<string>();
+            List<string> curvars2 = new List<string>();
+            curvars1.Add(textBox1.Text);
+            curvars1.Add(textBox2.Text);
+            curvars1.Add(textBox3.Text);
+            curvars1.Add(textBox4.Text);
+            curvars1.Add(textBox5.Text);
+            curvars1.Add(textBox6.Text);
+            curvars1.Add(textBox7.Text);
+            curvars1.Add(textBox8.Text);
+            curvars1.Add(textBox9.Text);
+            curvars1.Add(textBox10.Text);
+            curvars2.Add(textBox11.Text);
+            curvars2.Add(textBox12.Text);
+            curvars2.Add(textBox13.Text);
+            curvars2.Add(textBox14.Text);
+            curvars2.Add(textBox15.Text);
+            curvars2.Add(textBox16.Text);
+            curvars2.Add(textBox17.Text);
+            curvars2.Add(textBox18.Text);
+            curvars2.Add(textBox19.Text);
+            curvars2.Add(textBox20.Text);
+            List<double> var1DecList= new List<double>();
+            List<double> var2DecList = new List<double>();
+            for (int i = 0; i < 10; i++)
+            {
+                if (curvars1[i] == "" && curvars2[i] == "") { continue; }
+                else if (curvars1[i] == "" || curvars2[i] == "")
+                {
+                    MessageBox.Show("Error with input. Try again!");  // This will be replaced with an error func
+                }
+                else
+                {
+                    try
+                    {
+                        var1DecList.Add(Convert.ToDouble(curvars1[i]));
+                        var2DecList.Add(Convert.ToDouble(curvars2[i]));
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error with input. Try again!");  // This will be replaced with an error func
+                    }
+                }
+            }
+
+            // This next chunk will get percentage data from form as a list of decimals, with max length being 13
+            List<double> percentages1 = new List<double>();
+            List<double> actualPercentages1 = new List<double>();
+            percentages1.Add(Convert.ToDouble(numericUpDown1.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown2.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown3.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown4.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown5.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown6.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown7.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown8.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown9.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown10.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown11.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown12.Value));
+            percentages1.Add(Convert.ToDouble(numericUpDown13.Value));
+            for (int i = 0; i < 13; i++)
+            {
+                if (percentages1[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    actualPercentages1.Add(percentages1[i] / (double)100);
+                }
+            }
+            calculator jim = new calculator();
+            for (int cur_vars_index = 0; cur_vars_index < var1DecList.Count; cur_vars_index++)
+            {
+                double var1 = var1DecList[cur_vars_index];
+                double var2 = var2DecList[cur_vars_index];
+                string cur_file = filename + "-" +var1.ToString() + "-" + var2.ToString() + ".csv";
+                double answer = jim.Calculate(cur_file, trials, sample_size, var1, var2, actualPercentages1);
+            }
+            MessageBox.Show("Calculated. Check folder: " + System.IO.Path.GetDirectoryName(Application.ExecutablePath));
+        }
+
+        private void textBox21_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filenameBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox21_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsCVA
+{
+    public class calculator
+    {
+        public calculator()
+        {
+        }
+
+        public Random r = new Random();
+
+        public List<double> Modelmaker(double var1, double var2, double percent, int sample_size)
+        {
+            List<double> model = new List<double>();
+
+            for (int cur_data_index = 0; cur_data_index < sample_size; cur_data_index++)
+            {
+                double curcutoff = r.NextDouble();
+                if (curcutoff < percent) { model.Add(var1); }
+                else { model.Add(var2); }
+            }
+            return model;
+        }
+
+        public double Calculate(string filename, int trials, int sample_size, double var1, double var2, List<double> percentages)
+        {
+            List<List<double>> cur_sums_list_list = new List<List<double>>();
+            foreach (double percent in percentages)
+            {
+                List<double> cur_sums_list = new List<double>();
+                for (int trial = 0; trial < trials; trial++)
+                {
+                    List<double> cur_model = Modelmaker(var1, var2, percent, sample_size);
+                    double cur_sum = cur_model.Sum();
+                    cur_sums_list.Add(cur_sum);
+                }
+                cur_sums_list_list.Add(cur_sums_list);
+            }
+
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                // Get heading for CSV file and write
+                string heading = " ,";
+                foreach (double percent in percentages)
+                {
+                    heading += ((percent * 100).ToString() + ",");
+                }
+                heading = heading.TrimEnd(',');
+                sw.WriteLine(heading);
+
+                for (int trial = 0; trial < trials; trial++)
+                {
+                    string cur_row = trial.ToString() + ",";
+                    for (int percent_index = 0; percent_index < percentages.Count; percent_index++)
+                    {
+                        cur_row += (cur_sums_list_list[percent_index][trial].ToString() + ",");
+                    }
+                    cur_row = cur_row.TrimEnd(',');
+                    sw.WriteLine(cur_row);
+
+                }
+                // All the precious data analysis
+                List<double> all_stnds = new List<double>();
+                List<double> all_means = new List<double>();
+                List<double> all_cvs = new List<double>();
+                List<double> all_xp2s = new List<double>();
+                // All the row strings
+                string means_row = "Mean,";
+                string stnds_row = "Standard Deviation,";
+                string cvs_row = "% CV,";
+                string xp2s_row = "x + 2s,";
+                for (int percent_index = 0; percent_index < percentages.Count; percent_index++)
+                {
+                    // Get the mean
+                    double cur_mean = cur_sums_list_list[percent_index].Average();
+                    all_means.Add(cur_mean);
+                    means_row += (cur_mean.ToString() + ",");
+                    // Get the standard deviation
+                    double numerator = cur_sums_list_list[percent_index].Sum(d => Math.Pow(d - cur_mean, 2));
+                    double cur_stnd = Math.Sqrt((numerator) / (trials)); // This line may be wrong, check later
+                    all_stnds.Add(cur_stnd);
+                    stnds_row += (cur_stnd.ToString() + ",");
+                    // Get the coefficient of variation
+                    double cur_cv = cur_stnd / cur_mean * 100;
+                    all_cvs.Add(cur_cv);
+                    cvs_row += (cur_cv.ToString() + ",");
+                    // Get 'x + 2s'
+                    double cur_xp2s = cur_mean + (2 * cur_stnd);
+                    all_xp2s.Add(cur_xp2s);
+                    xp2s_row += (cur_xp2s.ToString() + ",");
+                }
+                // Format row strings correctly and finally write
+                means_row = means_row.TrimEnd(',');
+                sw.WriteLine(means_row);
+
+                stnds_row = stnds_row.TrimEnd(',');
+                sw.WriteLine(stnds_row);
+
+                cvs_row = cvs_row.TrimEnd(',');
+                sw.WriteLine(cvs_row);
+
+                xp2s_row = xp2s_row.TrimEnd(',');
+                sw.WriteLine(xp2s_row);
+            }
+            return 1;
+        }
+    }
+
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
